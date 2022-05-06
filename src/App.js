109f813631ee8cb0fd3f01abc095675d280706e9
Vladislav Kostromin <61909342+vladislavkostromin@users.noreply.github.com>
2022-05-06 20:50:02 +0500

    add routs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import {Switch, Route} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+
 function App() {
   return (
-    <div className="App">
-      app
-    </div>
+    <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/login' component={LoginPage}/>
+        <Route exact path='/register' component={RegisterPage}/>
+    </Switch>
   );
 }
 
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import{Redirect} from "react-router-dom";
+
+const HomePage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HomePage;
