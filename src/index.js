6a6d7ab59601b3d6f6c3aa13839c6aa20c9efca8
Vladislav Kostromin <61909342+vladislavkostromin@users.noreply.github.com>
2022-05-16 13:16:12 +0500

    useAuth/add firebase

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {BrowserRouter} from "react-router-dom"
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
+import { store } from './store'
+import './firebase'
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<BrowserRouter><App /></BrowserRouter>)
+root.render
+(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider> 
+  </BrowserRouter>
+)
 
